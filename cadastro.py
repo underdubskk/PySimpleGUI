f41eaa99f9975bcd490188c406728114d48c0b1e
under <stevenfadublagem@gmail.com>
2024-08-13 11:02:06 -0300

    first commit

---
+++ cadastro.py
@@ -0,0 +1,22 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+# Layout
+sg.theme('Dark Blue 3')
+layout = [
+    [sg.Text('Usuário'),sg.Input(key='usuário')],
+    [sg.Text('Senha'),sg.Input(key='senha',password_char='*')],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')]
+]
+
+# Janela
+janela = sg.Window('Tela de Login', layout)
+
+# Ler eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'Cleitin' and valores['senha'] == '123456':
+            print('Seja Bem-vindo Cleitin!')
